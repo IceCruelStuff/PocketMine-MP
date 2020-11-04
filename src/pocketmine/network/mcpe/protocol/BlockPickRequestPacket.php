fa5023afbc0e6ee14d8a5aa7fd121662de56ff4a
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-03 22:37:12 -0800

    Update BlockPickRequestPacket.php

--- src/pocketmine/network/mcpe/protocol/BlockPickRequestPacket.php
+++ src/pocketmine/network/mcpe/protocol/BlockPickRequestPacket.php
@@ -41,13 +41,13 @@ class BlockPickRequestPacket extends DataPacket{
 	/** @var int */
 	public $hotbarSlot;
 
-	protected function decodePayload(){
+	protected function decodePayload(int $playerProtocol){
 		$this->getSignedBlockPosition($this->blockX, $this->blockY, $this->blockZ);
 		$this->addUserData = $this->getBool();
 		$this->hotbarSlot = $this->getByte();
 	}
 
-	protected function encodePayload(){
+	protected function encodePayload(int $playerProtocol){
 		$this->putSignedBlockPosition($this->blockX, $this->blockY, $this->blockZ);
 		$this->putBool($this->addUserData);
 		$this->putByte($this->hotbarSlot);
