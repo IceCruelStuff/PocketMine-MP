2642a24c76091d603e9db200f3fc8386ad5cd54e
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-22 12:01:28 -0800

    Update TransferPacket.php

--- src/pocketmine/network/mcpe/protocol/TransferPacket.php
+++ src/pocketmine/network/mcpe/protocol/TransferPacket.php
@@ -35,12 +35,12 @@ class TransferPacket extends DataPacket{
 	/** @var int */
 	public $port = 19132;
 
-	protected function decodePayload(){
+	protected function decodePayload(int $playerProtocol){
 		$this->address = $this->getString();
 		$this->port = $this->getLShort();
 	}
 
-	protected function encodePayload(){
+	protected function encodePayload(int $playerProtocol){
 		$this->putString($this->address);
 		$this->putLShort($this->port);
 	}
